be96c70b7b71adad152d362c257646c7435d7c1f
lhzhang <lhzhang@live.unc.edu>
2020-06-20 21:21:47 -0400

    Working on Checkboxes

--- src/components/TimelineFilter.tsx
+++ src/components/TimelineFilter.tsx
@@ -25,7 +25,7 @@ class TimelineFilter extends Component<Prop, State> {
             <Card className="Filter">
                 <CardContent>
                     <TextField fullWidth id="outlined-basic" label="Search" variant="outlined" onChange={this.onFieldChange} />
-                    <FormControl fullWidth>
+                    <FormControl fullWidth component="fieldset">
                         <FormGroup row>
                             {this.props.langList.map((lang: string) => (
                                 <FormControlLabel control={<Checkbox name={lang} />} label={lang} />
