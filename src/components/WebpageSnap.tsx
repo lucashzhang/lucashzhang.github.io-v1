e6262d4aa96ee6f9403530b8796c43354cbbe617
lhzhang <lhzhang@live.unc.edu>
2020-06-22 17:59:58 -0400

    Now supports just playing from raw.githubusercontent

--- src/components/WebpageSnap.tsx
+++ src/components/WebpageSnap.tsx
@@ -13,10 +13,15 @@ interface State {
 class WebpageSnap extends Component<Prop, State> {
 
     render() {
+
         const apiURL: string = 'https://api.apiflash.com/v1/urltoimage?access_key=81b584f60fb2444db5beb297443320db&ttl=2592000&url=';
+        const userContent: string = "raw.githubusercontent.com/";
         const snapImgURL: string = apiURL + this.props.url;
 
         if (this.props.url != null && this.props.url.length > 0) {
+            if (this.props.url.includes(userContent)) {
+                return <img src={this.props.url} alt="" />;
+            }
             return <img src={snapImgURL} alt="" />
         } else {
             return <img src={placeholder} alt="" />;
