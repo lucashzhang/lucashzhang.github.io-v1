2415970fc9adcd42b2ce384c9d9a6915a1192ce6
lhzhang <lhzhang@live.unc.edu>
2020-06-26 13:00:06 -0400

    Colors no longer randomly shuffled, uses logic to avoid putting simlilar colors next to each other

--- src/utilities/handleColors.ts
+++ src/utilities/handleColors.ts
@@ -1,11 +1,14 @@
-import { shuffle } from './generalUtil';
-
 export function assignColors(uniqLang: string[]) {
     let colorDict: { [index: string]: any; } = {};
 
-    let langs = shuffle(uniqLang);
+    //Goes through every other one in order so colors that are too similar aren't next to each other
 
-    for (let lang in langs) {
+    for (let lang = 0; lang < uniqLang.length; lang+=2) {
+        let currLang: string = uniqLang[lang];
+        colorDict[currLang] = selectColors(lang, uniqLang.length);
+    }
+
+    for (let lang = 1; lang < uniqLang.length; lang+=2) {
         let currLang: string = uniqLang[lang];
         colorDict[currLang] = selectColors(lang, uniqLang.length);
     }
