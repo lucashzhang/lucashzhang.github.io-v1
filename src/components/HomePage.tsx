1ed7b6172c859e513fe2a582c870d21d2d040392
lhzhang <lhzhang@live.unc.edu>
2020-07-06 17:47:16 -0400

    Added new logo (prototype) to page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import HomePageIntro from './HomePageIntro';
 import HomePageAbout from './HomePageAbout';
 import HomePageContact from './HomePageContact';
+import Logo from '../assets/Logo20.png';
 import '../css/HomePage.css';
 
 export interface Prop {
@@ -15,9 +16,10 @@ class DesignPage extends Component<Prop, State> {
 
     render = () => {
         return <div className="lucas-page">
-            <div className="homepage-home homepage-section">
+            {/* <div className="homepage-home homepage-section">
                 <h3 className="homepage-big-text">Hi, I'm Lucas Zhang</h3>
-            </div>
+            </div> */}
+            <img className="homepage-home homepage-section" id="homepage-logo" src={Logo} alt="Hi, I'm Lucas Zhang"></img>
             <HomePageIntro></HomePageIntro>
             <hr className="homepage-divider"/>
             <HomePageAbout></HomePageAbout>
