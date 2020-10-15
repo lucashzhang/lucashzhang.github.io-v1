a3b463d2cf9c0e7af3e389793ad06d4eafafc03b
Lucas Zhang <lhzhang@live.unc.edu>
2020-10-15 12:19:46 -0400

    Tweaked visibility sensors on main page

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -25,11 +25,11 @@ const Main = _ => {
     return (
         <ThemeProvider theme={theme}>
             <Redirect to={visible}/>
-            <VizSensor partialVisibility minTopValue = {750} onChange={(isVisible) => {if (isVisible) setVisible('/#home')}}><Home/></VizSensor>
+            <VizSensor partialVisibility onChange={(isVisible) => {if (isVisible) setVisible('/#home')}}><Home/></VizSensor>
             <section className={classes.divider}></section>
-            <VizSensor partialVisibility minTopValue = {750} onChange={(isVisible) => {if (isVisible) setVisible('/#about')}}><About/></VizSensor>
+            <VizSensor partialVisibility onChange={(isVisible) => {if (isVisible) setVisible('/#about')}}><About/></VizSensor>
             <section className={classes.divider}></section>
-            <VizSensor partialVisibility minTopValue = {750} onChange={(isVisible) => {if (isVisible) setVisible('/#contact')}}><Contact/></VizSensor>
+            <VizSensor partialVisibility onChange={(isVisible) => {if (isVisible) setVisible('/#contact')}}><Contact/></VizSensor>
         </ThemeProvider>
     )
 }
