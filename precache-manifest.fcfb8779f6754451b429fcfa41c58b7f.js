ac3b7efd7c8c502175c174277c9e29711a1ce0a6
Lucas Zhang <lhzhang@live.unc.edu>
2020-12-26 21:55:31 -0500

    Updates

---
+++ precache-manifest.fcfb8779f6754451b429fcfa41c58b7f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb4cab9dd4c371183003e5e0a2da4f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09e2b0080044a828933b",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "f2d8d4f56c52b76f4ba0",
+    "url": "/static/css/main.f9ae596f.chunk.css"
+  },
+  {
+    "revision": "09e2b0080044a828933b",
+    "url": "/static/js/2.160af567.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.160af567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d8d4f56c52b76f4ba0",
+    "url": "/static/js/main.d95d478b.chunk.js"
+  },
+  {
+    "revision": "2c194140c46ae51d6f05",
+    "url": "/static/js/runtime-main.f1dea56d.js"
+  },
+  {
+    "revision": "4069e7dcc2fa06a94303c1fd5d0c0ce1",
+    "url": "/static/media/Code-Placeholder.4069e7dc.png"
+  },
+  {
+    "revision": "5d939f81b0f74e9e1f3726efb42afde8",
+    "url": "/static/media/Headshot.5d939f81.jpg"
+  },
+  {
+    "revision": "b6f32f5788aa042632f2f6272a7bffe0",
+    "url": "/static/media/NeonLogo.b6f32f57.svg"
+  },
+  {
+    "revision": "b4c3a4ab0d27e11ed11dd6ced1bb21c6",
+    "url": "/static/media/WebsiteBackground-01.b4c3a4ab.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddb20adf4afa542610049079d54a41f2.js"
+  "/precache-manifest.fcfb8779f6754451b429fcfa41c58b7f.js"
 );
 
 self.addEventListener('message', (event) => {
